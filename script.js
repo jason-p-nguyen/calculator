847166444e028005cfd18bd4c547a219e8b8a68b
Jason Nguyen <29442130+jason-p-nguyen@users.noreply.github.com>
2026-02-17 13:36:53 +0900

    Add and test basic math functions

---
+++ script.js
@@ -0,0 +1,24 @@
+//add
+function add(a, b) {
+    return a + b
+}
+
+//subtraction
+function subtract(a, b) {
+    return a - b
+}
+
+//multiply
+function multiply(a, b) {
+    return a * b
+}
+
+//divide
+function divide(a, b) {
+    return a / b
+}
+
+console.log(add(4, 5));
+console.log(subtract(5, 3));
+console.log(multiply(4, 9));
+console.log(divide(10, 2));

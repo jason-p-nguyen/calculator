b2a5901b00ac6706f02d0780367c8a37281c5a73
Jason Nguyen <29442130+jason-p-nguyen@users.noreply.github.com>
2026-02-19 22:13:32 +0900

    Set buttons to input into JS variables

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let num1 = null;
-let operator = null;
-let num2 = null;
+let num1;
+let operator;
+let num2;
 
 //add
 function add(a, b) {
@@ -39,14 +39,29 @@ function operate(num1, num2, operator) {
         return "ERROR";
     }
 }
+
+console.log(num1);
+console.log(num2);
+console.log(operator);
+
 //set listener to button "1"
+//set num1 to "1"
+//test: output "num1 = 1" on the console log
 const button = document.querySelectorAll("button");
 console.log(button)
 button.forEach(button => button.addEventListener("click", (event) => {
-    console.log(button.value);
+    if (num1 === undefined) {
+        num1 = button.value;
+        console.log(num1);
+        return num1
+    } else if (num2 === undefined) {
+        num2 = button.value;
+        console.log(num2);
+        return num2;
+    }
 }) )
-//set num1 to "1"
-//test: output "num1 = 1" on the console log
+
+
 //set listener to button "+"
 //set operator to "+"
 //test: output "operator = +" on console log

11e752f90df6e97307e7ea4b0cf0aee8a571789c
Jason Nguyen <29442130+jason-p-nguyen@users.noreply.github.com>
2026-02-20 15:00:37 +0900

    FIX: operate() now outputs number result

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let num1 = [];
-console.log(num1);
+let parsedNum1;
 let operator;
 let num2 = [];
+let parsedNum2;
 let results;
 
 function add(a, b) {
-    return a + b
+    return a + b;
 };
 
 function subtract(a, b) {
-    return a - b
+    return a - b;
 };
 
 function multiply(a, b) {
-    return a * b
+    return a * b;
 };
 
 function divide(a, b) {
-    return a / b
+    return a / b;
 };
 
 function operate(num1, num2, operator) {
@@ -38,30 +39,25 @@ function operate(num1, num2, operator) {
     };
 };
 
-
-
 const numbers = document.querySelectorAll(".number");
 numbers.forEach(number => number.addEventListener("click", (event) => {
     if (operator === undefined) {
         num1.push(number.value);
         let joinedNum1 = num1.join("");
-        joinedNum1 = parseInt(joinedNum1);
         display.textContent = joinedNum1;
         console.log(joinedNum1, typeof joinedNum1);
-        return parseInt(joinedNum1);
+        return parsedNum1 = parseInt(joinedNum1);
 
     } else if (operator !== undefined) {
         num2.push(number.value);
         let joinedNum2 = num2.join("");
-        joinedNum2 = parseInt(joinedNum2);
         display.textContent = joinedNum2;
         console.log(joinedNum2, typeof joinedNum2);
-        return parseInt(joinedNum2);
+        return parsedNum2 = parseInt(joinedNum2);
     } else {
         display.textContent = "ERROR";
     };
 }));
-
 
 const maths = document.querySelectorAll(".maths");
 maths.forEach(math => math.addEventListener("click", (event) => {
@@ -73,13 +69,13 @@ maths.forEach(math => math.addEventListener("click", (event) => {
 const equal = document.querySelector(".equal");
 const display = document.querySelector("#display");
 equal.addEventListener("click", (event) => {
-    results = operate(num1, num2, operator);
+    results = operate(parsedNum1, parsedNum2, operator);
     console.log(`results: ${results}`);
     display.textContent = results;
-    num1 = results;
-    console.log(`num1: ${num1}`);
-    num2 = undefined;
-    console.log(`num2: ${num2}`);
+    parsedNum1 = results;
+    console.log(`num1: ${parsedNum1}`);
+    parsedNum2 = undefined;
+    console.log(`num2: ${parsedNum1}`);
 });
 
 // Clear results when pushing clear button
